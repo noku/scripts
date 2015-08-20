9d17a95e6500a619f854d2e89347d12aa7c99215
Peter Negrei <negrei.petru@gmail.com>
2015-08-20 12:51:45 +0300

    refactor images

--- img_donwloader/images.rb
+++ img_donwloader/images.rb
@@ -7,63 +7,115 @@ require 'colorize'
 require 'net/http'
 require 'json'
 
-class Wallpapers
-  attr_reader :uri, :links, :images
+module Downloader
+  # Download and save images from Wallbase
+  class Images
+    attr_reader :logger, :image_links
+    JSON_PATH = 'link.json'
 
-  def initialize
-    @uri = URI('http://alpha.wallhaven.cc/search?categories=111&purity=110&ratios=16x9,16x10&sorting=views&order=desc')
-    @links = []
-    @images = {}
-    @counter, @counter_img = 0, 0
+    def initialize(logger = Logger)
+      # need to add check if backup is present
+      @image_links ||= LinkBuilder.new.image_links
+      @logger = logger
+      @image_counter = 0
+
+      save_backup_json
+    end
+
+    def save
+      image_links.each { |src| save_image(src) }
+    end
+
+    private
+
+    def save_backup_json
+      File.write(JSON_PATH, JSON.generate(image_links))
+    end
+
+    def file_format_for(src)
+      src.match(/\.\w*$/)[0]
+    end
+
+    def save_image(link)
+      res = Net::HTTP.get_response(URI(link))
+      save_to_file(file_format_for(link), res.body) if success?(res)
+    end
+
+    def save_to_file(format, data)
+      logger.red_notification
+      file = File.new("#{@image_counter += 1}#{format}", 'wb')
+      file.puts(data)
+      file.close
+    end
+
+    def success?(response)
+      response.is_a?(Net::HTTPSuccess)
+    end
   end
 
-  def save
-    access_links
-    File.write('link.json',  JSON.generate(@images))
-    @images.each {|name, src| save_image src.match(/\.\w*$/)[0], src }
+  # Find all Image Links from source
+  class LinkBuilder
+    attr_reader :image_links, :logger
+
+    def initialize(logger = Logger)
+      @image_links = []
+      @logger = logger
+
+      prepare_images
+    end
+
+    def prepare_images
+      address_of_links.each { |link| analyze_for_images!(link) }
+    end
+
+    def address_of_links
+      main_page.css(link_location).map { |link| link['href'] }
+    end
+
+    def analyze_for_images!(link)
+      logger.green_notification
+      wall_for_image(link).each { |img_link| add_to_img_links(img_link) }
+    end
+
+    private
+
+    def main_page
+      Nokogiri::HTML(main_body)
+    end
+
+    def wall_for_image(link)
+      Nokogiri::HTML(request_body(link)).css('#showcase>img')
+    end
+
+    def add_to_img_links(img_link)
+      @image_links << "http:#{img_link['src']}"
+    end
+
+    def main_body
+      File.read('source.html')
+    end
+
+    def link_location
+      "section[class='thumb-listing-page']>ul>li>figure>a"
+    end
+
+    def request_body(link)
+      uri = URI(link)
+      res = Net::HTTP.get_response(uri)
+      res.body if res.is_a?(Net::HTTPSuccess)
+    end
   end
 
-  def access_links
-    adress_links.each { |link| analyze_for_images(link) }
+  # Output color line for progress indication
+  class Logger
+    def self.green_notification
+      print '|'.green
+    end
+
+    def self.red_notification
+      print '|'.red
+    end
   end
-
-  def main_body
-    File.read("source.html")
-  end
-
-  private
-
-  def adress_links
-    wall = Nokogiri::HTML(main_body)
-    wall.css("section[class='thumb-listing-page']>ul>li>figure>a").map { |link| link['href']}
-  end
-
-  def analyze_for_images link
-    print "|".green
-    wall = Nokogiri::HTML(image_body(link))
-    wall.css("#showcase>img").each { |link|  @images[link['data-wallpaper-id']] ="http:" << link['src'] }
-  end
-
-  def image_body link
-    uri = URI(link)
-    res =  Net::HTTP.get_response(uri)
-    res.body if res.is_a?(Net::HTTPSuccess)
-  end
-
-  def save_image format, link
-    uri = URI(link)
-    res =  Net::HTTP.get_response(uri)
-    save_to_file(format, res.body) if res.is_a?(Net::HTTPSuccess)
-  end
-
-  def save_to_file(format, data)
-    print "|".red
-    # print "[#{@counter_img += 1}|#{@images.size}]".red
-    file = File.new("#{@counter_img += 1}#{format}", 'wb')
-    file.puts(data)
-    file.close
-  end
-
 end
 
-Wallpapers.new.save
+Downloader::Images.new.save
